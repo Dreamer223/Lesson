c5dfd899cca4a162c5ccdc8332c43aa6abfecc69
Dreamer223 <drackfil@gmail.com>
2023-09-04 13:34:52 +0300

    Рабочая версия

--- src/main/java/task3/Game/Main.java
+++ src/main/java/task3/Game/Main.java
@@ -1,16 +1,22 @@
 package task3.Game;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String restart = "1";
+        String answer2 = null;
+        int otv;
+        List<String> statistic = new ArrayList<>();
         while (restart.equals("1")){
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Выберите режим игры:" +
-                    "\n Русские слова - 1" +
-                    "\n Англиские слова - 2" +
-                    "\n Числа - 3");
+            System.out.println("""
+                    Выберите режим игры:
+                     Русские слова - 1
+                     Англиские слова - 2
+                     Числа - 3""");
             int gameMode = Integer.parseInt(scanner.nextLine());
             Game game = null;
             switch (gameMode) {
@@ -26,16 +32,23 @@ public class Main {
                     && !game.getGameStatus().equals(GameStatus.GAME_OVER)){
                 String scanWord = scanner.nextLine();
                 Answer answer = game.inputValue(scanWord);
-                System.out.println("answer = " + answer);
+                statistic.add(String.valueOf(answer));
             }
             System.out.println(game.getGameStatus());
+            System.out.println("Хотите посмотреть свою статистику за эту игру?" +
+                    "Да - 1\t" + "Нет - 0\n");
+            otv = scanner.nextInt();
+            if (otv == 1){
+                System.out.println(statistic);
+//                    TimeUnit.SECONDS.sleep(5);
+            }
             System.out.println("Вы хотите начать игру занова\n" +
                     "Да - 1\t" + "Нет - 0\n");
             restart = scanner.nextLine();
-
-            if (restart == "0"){
-                break;
+            if (restart.equals("0")){
+                System.exit(0);
             }
         }
+
     }
 }
